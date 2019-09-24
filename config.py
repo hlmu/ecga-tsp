5cafcf2db204b3b1817422bdc5e3c80c493050f5
mhlwsk <muhonglinyt@126.com>
2019-09-24 23:00:53 +0800

    Change to checkpoint mode

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ Configuration file
 POP_SIZE = 50      #每一代个体数
 ELITE_SIZE = 10      #保留的精英个体数量
 MUTATE_RATE = 0.002    #基因变异概率
-N_GENERATIONS = 100   #生成子代数量
-CHECKPOINTS = [10, 50, 100]
+N_GENERATIONS = 20000   #生成子代数量
+CHECKPOINTS = [5000, 10000, 20000]
