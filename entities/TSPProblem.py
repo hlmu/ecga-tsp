26c4489d0a314eb71aa3cc5093bcd118b9cba4a7
mhlwsk <muhonglinyt@126.com>
2019-09-25 21:07:30 +0800

    Modify logging format.

--- entities/TSPProblem.py
+++ entities/TSPProblem.py
@@ -73,7 +73,7 @@ class TSPProblem:
         population = self.initial_population(pop_size, self.citylist)
         type = os.path.splitext(os.path.basename(self.filename))[0]
         logfile = 'logs/' + type + '-' + self.alg.name + '-' + str(pop_size) + '.txt'
-        logging.basicConfig(filename=logfile, level=logging.INFO)
+        logging.basicConfig(filename=logfile, filemode='w', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
         logging.info('Progress started, population size: {}, elite size: {}, mutation rate: {}, total generation: {}\n'
                      .format(pop_size, elite_size, mutation_rate, generation))
         for gen in range(1, generation+1):
